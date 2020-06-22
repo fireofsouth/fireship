cbe5df162e9e1860c224d30be12d03b95c3a5964
hufangyi <hufangyi@taou.com>
2020-06-22 10:42:04 +0800

    删掉dist

--- dist/components/Menu/menuItem.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export interface MenuItemProps {
-    index?: string;
-    disabled?: boolean;
-    className?: string;
-    style?: React.CSSProperties;
-}
-export declare const MenuItem: React.FC<MenuItemProps>;
-export default MenuItem;
--- dist/components/Upload/progress.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ThemeProps } from '../Icon/icon';
-export interface ProgressProps {
-    percent: number;
-    strokeHeight?: number;
-    showText?: boolean;
-    styles?: React.CSSProperties;
-    theme?: ThemeProps;
-}
