0e66b929cbf6ee4a35d5a01f8ba23e5d81644913
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-27 08:44:16 +0000

    Deploy fireofsouth/fireship to github.com/fireofsouth/fireship.git:gh-pages

---
+++ precache-manifest.7a5223244d8388dd9b0358eb32850f26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4833e518c40c0616ebe38eea111a2e52",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.71c5c3526502fa3696ad.bundle.js"
+  },
+  {
+    "url": "runtime~main.71c5c3526502fa3696ad.bundle.js"
+  },
+  {
+    "revision": "3629cbf16a9c3726a282",
+    "url": "static/css/main.7e290629.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.71c5c3526502fa3696ad.bundle.js"
+  },
+  {
+    "url": "vendors~main.71c5c3526502fa3696ad.bundle.js.LICENSE.txt"
+  }
+]);
