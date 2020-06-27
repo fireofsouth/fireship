18fe1d5dee6a7773b878741462942f150c8936c5
hufangyi <519954072@qq.com>
2020-06-27 16:34:56 +0800

    配置网站

--- src/welcome.stories.tsx
+++ src/welcome.stories.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-import { storiesOf }  from '@storybook/react'
+import { storiesOf } from '@storybook/react';
 
-storiesOf('Welcome page', module)
-  .add('welcome', () => {
+storiesOf('Welcome page', module).add(
+  'welcome',
+  () => {
     return (
       <>
         <h1>欢迎来到 fireship 组件库</h1>
-        <p>fireship 用起来</p>
+        <p>发布到github上</p>
         <h3>安装试试</h3>
-        <code>
-          npm install fireship --save
-        </code>
+        <code>npm install fireship --save</code>
       </>
-    )
-  }, { info : { disable: true }})
+    );
+  },
+  { info: { disable: true } }
+);
