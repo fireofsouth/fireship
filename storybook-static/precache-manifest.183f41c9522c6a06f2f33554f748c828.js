4dc8c9e780410c53cc74324ec70d0e75bc9966e2
hufangyi <519954072@qq.com>
2020-06-27 16:24:51 +0800

    发布页面

---
+++ storybook-static/precache-manifest.183f41c9522c6a06f2f33554f748c828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88cd4ef475b16d9fec3c2bc4dba9d02f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c04d9e9d94ce2fc4b719.bundle.js"
+  },
+  {
+    "url": "runtime~main.c04d9e9d94ce2fc4b719.bundle.js"
+  },
+  {
+    "revision": "d8ac8c43d9abd048374b",
+    "url": "static/css/main.26c48539.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.c04d9e9d94ce2fc4b719.bundle.js"
+  },
+  {
+    "url": "vendors~main.c04d9e9d94ce2fc4b719.bundle.js.LICENSE.txt"
+  }
+]);
